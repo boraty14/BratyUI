5fa84e65b5a67ff488bb8df68fae5e152d034c79
braty <bataysen@zynga.com>
2023-10-12 17:32:12 +0300

    text update

--- Assets/Scripts/BratyUI/TextInput.cs
+++ Assets/Scripts/BratyUI/TextInput.cs
@@ -2,20 +2,44 @@ using System;
 using BratyUI.Attributes;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace BratyUI
 {
     [RequireComponent(typeof(TMP_Text))]
-    public class TextInput : ComponentBase
+    public class TextInput : InteractableBase,IPointerClickHandler
     {
         [SerializeField] [ShowOnly] private TMP_Text _text;
+        [SerializeField] private string _placeHolder;
+        private TouchScreenKeyboard _keyboard;
+        private string _currentText;
 
-        private void OnValidate()
+        private void OnEnable()
         {
+            _text.text = _placeHolder;
+        }
+
+        protected override void OnValidate()
+        {
+            base.OnValidate();
             if (_text == null)
             {
                 _text = GetComponent<TMP_Text>();
             }
+        }
+
+        private void Update()
+        {
+            if (_keyboard == null)
+            {
+                return;
+            }
+            
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            
         }
     }
 }
